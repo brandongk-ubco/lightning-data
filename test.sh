a093fd949d0daa2f7196e061e6c9fa686693ec49
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2022-03-15 15:02:08 -0700

    Fixing.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m pytest --cov=gitstats --cov-branch --cov-report term-missing --cov-fail-under=90
+python -m pytest --cov=lightningdata --cov-branch --cov-report term-missing --cov-fail-under=90
