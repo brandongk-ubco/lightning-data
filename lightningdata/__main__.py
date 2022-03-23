b6282aae45dcfc7da0c484d733be2e6c1af2ead8
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2022-03-23 14:09:38 -0700

    Fixing commands.

--- lightningdata/__main__.py
+++ lightningdata/__main__.py
@@ -1,8 +1,11 @@
 import argh
 from lightningdata import commands
+from inspect import getmembers, isfunction
+
+command_list = [o[1] for o in getmembers(commands) if isfunction(o[1])]
 
 parser = argh.ArghParser()
-parser.add_commands(commands.__all__)
+parser.add_commands(command_list)
 
 if __name__ == '__main__':
     parser.dispatch()
