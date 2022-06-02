1fc995872717409132a19f213bbf19ebdff35b86
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2022-06-02 13:57:29 -0700

    Simplifying requirements and supporting only Python 3.8+

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setuptools.setup(name="lightningdata",
                      "License :: OSI Approved :: MIT License",
                      "Operating System :: OS Independent",
                  ],
-                 python_requires='>=3.6',
+                 python_requires='>=3.8',
                  include_package_data=True)
