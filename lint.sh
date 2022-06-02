1248a85bbde65c425647e7a8c39c53a2602c8030
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2022-06-02 14:09:52 -0700

    Fixing coverage requirement.

--- lint.sh
+++ lint.sh
@@ -2,5 +2,5 @@
 
 mypy lightningdata
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 . --count  --max-complexity=10 --statistics
+flake8 . --count  --max-complexity=15 --statistics
 yapf --diff -r lightningdata
