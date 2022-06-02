ee1f9dc39b74a37f87174adf14a4bf1680dea126
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2022-06-02 10:06:27 -0700

    Loading modules on init.

--- lightningdata/__init__.py
+++ lightningdata/__init__.py
@@ -1,3 +1,14 @@
+import os
+import json
+import importlib
 from . import vision
+
+modules_file = os.path.join(os.getcwd(), "modules.json")
+if os.path.exists(modules_file):
+    with open(modules_file, "r") as jsonfile:
+        modules = json.load(jsonfile)
+    if "datasets" in modules:
+        for module in modules["datasets"]:
+            importlib.import_module(module)
 
 __all__ = ["vision"]
--- lightningdata/__main__.py
+++ lightningdata/__main__.py
@@ -1,9 +1,6 @@
 import argh
 from lightningdata import commands
 from inspect import getmembers, isfunction
-import os
-import json
-import importlib
 
 command_list = [o[1] for o in getmembers(commands) if isfunction(o[1])]
 
@@ -11,11 +8,4 @@ parser = argh.ArghParser()
 parser.add_commands(command_list)
 
 if __name__ == '__main__':
-    modules_file = os.path.join(os.getcwd(), "modules.json")
-    if os.path.exists(modules_file):
-        with open(modules_file, "r") as jsonfile:
-            modules = json.load(jsonfile)
-        if "datasets" in modules:
-            for module in modules["datasets"]:
-                importlib.import_module(module)
     parser.dispatch()
